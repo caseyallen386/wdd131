09d533419547bd0d9ef9059f56e27bdf73ef84b4
Casey <casey.allen83@proton.me>
2026-08-31 18:44:35 -0600

    Added Copy and Changes to css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,6 @@
 (function() {
-    
+    const TODAY = new Date();
 
-    document.getElementById('last-modified').innerHTML = document.lastModified
+    document.getElementById('last-modified').innerHTML = "Last Modified: " + document.lastModified;
+    document.getElementById('currentyear').innerHTML = TODAY.getFullYear();
 })()
